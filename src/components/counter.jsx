e3c408095b5088f1c9882fa61adb703cc2309b8c
MeliiZa <melissazandman@icloud.com>
2020-06-02 12:59:27 -0700

    Display messaage when no tags available

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,30 +6,24 @@ class Counter extends Component {
         tags:["tag1","tag2","tag3"]
     };
 
-    
+    renderTags(){
+        if (this.state.tags.length === 0 ) return <p>There are no tags!</p>;
+
+        return<ul>
+        {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+          </ul>;
+    }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      <ul>
-    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-      </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+
       </div>
     );
   }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
 
-  formatCount(){
-      const { count } = this.state;
-      return count === 0 ? "Zero" : count;
-
-  }
 }
 
 export default Counter;
